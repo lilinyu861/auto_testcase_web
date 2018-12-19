d106a6e4b45e45968f5cfd20efb9274a9e5446ec
lily <lilinyu861@outlook.com>
2018-12-19 10:50:04 +0800

    refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,27 @@
 from django.test import TestCase
-
+# resolve 是django的内部函数用于解析url
+from django.urls import resolve
+from lists.views import home_page
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     # found.func返回所需要的示图函数
+    #     self.assertEqual(found.func, home_page)
+    #
+    # def test_home_page_returns_correct_html(self):
+    #     request = HttpRequest()
+    #     response = home_page(request)
+    #     html = response.content.decode('utf8')
+    #     excepted_html = render_to_string('home.html')
+    #     self.assertEqual(html, excepted_html)
+    #     # 检查相应是使用哪个模板渲染的
+    #     self.assertTemplateUsed(response, 'wrong_html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
--- test/function_tests.py
+++ test/function_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 import unittest
 
@@ -13,6 +14,19 @@ class NewVistorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('TO-DO', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('TO-DO', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to--do item'
+        )
+        inputbox.send_keys("buy a book")
+        inputbox.send_keys(Keys.ENTER)
+        sleep(1)
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: buy a book'for row in rows))
         # self.fail 都会失败，生成指定的错误信息，这里是为了提醒测试结束
         self.fail('Finish the test!')
 
